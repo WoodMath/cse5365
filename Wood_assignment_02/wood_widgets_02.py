7582e3583136a02bcfab386a598ddcee4401ccf8
JeffWood <jeffwood314@gmail.com>
2016-02-23 20:31:01 -0600

    Finished UI modifications using 'tkinter'

--- Wood_assignment_02/wood_widgets_02.py
+++ Wood_assignment_02/wood_widgets_02.py
@@ -9,6 +9,7 @@ from tkinter import messagebox
 from tkinter import simpledialog
 from tkinter import filedialog
 from wood_fileread_02 import *
+from tkValidatingEntry import *
 
 class cl_widgets:
     def __init__(self,ob_root_window,ob_world=[],ob_mesh=mesh()):
@@ -135,12 +136,93 @@ class cl_pannel_03:
         axis_frame = Frame(master.ob_root_window)
         axis_frame.pack()
 
-        i_axis = IntVar();
+        i_axis = IntVar(value=1)
 
-        self.axis_label = self.file_location_label = Label(axis_frame, text="Axis of Rotation").pack(side=LEFT,padx=10,pady=10)
+        self.axis_label = Label(axis_frame, text="Rotation").pack(side=LEFT,padx=10,pady=0)
         self.axis_x = Radiobutton(axis_frame, text="X", variable=i_axis, value=1).pack(side=LEFT,anchor=W)
         self.axis_y = Radiobutton(axis_frame, text="Y", variable=i_axis, value=2).pack(side=LEFT,anchor=W)
         self.axis_z = Radiobutton(axis_frame, text="Z", variable=i_axis, value=3).pack(side=LEFT,anchor=W)
+        self.rotate_ab = Radiobutton(axis_frame, text="Line AB", variable=i_axis, value=4).pack(side=LEFT,anchor=W, padx=10)
+
+        fDefaultZero = 0.0
+        fDefaultOne = 1.0
+        
+        fRotateAx = fDefaultZero
+        sRotateAx = StringVar(value=str(round(fRotateAx,2)))
+        fRotateAy = fDefaultZero
+        sRotateAy = StringVar(value=str(round(fRotateAy,2)))
+        fRotateAz = fDefaultZero
+        sRotateAz = StringVar(value=str(round(fRotateAz,2)))
+
+        fRotateBx = fDefaultOne
+        sRotateBx = StringVar(value=str(round(fRotateBx,2)))
+        fRotateBy = fDefaultOne
+        sRotateBy = StringVar(value=str(round(fRotateBy,2)))
+        fRotateBz = fDefaultOne
+        sRotateBz = StringVar(value=str(round(fRotateBz,2)))
+
+#        self.rotate_ax.delete(0, END)
+#        self.rotate_ax.insert(0, "1.0")
+        
+        self.rotate_a_label = Label(axis_frame, text="A:").pack(side=LEFT,padx=0,pady=0)
+        self.rotate_ax = Entry(axis_frame, textvariable=sRotateAx, width=3).pack(side=LEFT,padx=0, pady=0)
+        self.rotate_ay = Entry(axis_frame, textvariable=sRotateAy, width=3).pack(side=LEFT,padx=0, pady=0)
+        self.rotate_az = Entry(axis_frame, textvariable=sRotateAz, width=3).pack(side=LEFT,padx=0, pady=0)
+        
+        self.rotate_b_label = Label(axis_frame, text="B:").pack(side=LEFT,padx=0,pady=0)
+        self.rotate_bx = Entry(axis_frame, textvariable=sRotateBx, width=3).pack(side=LEFT,padx=0, pady=0)
+        self.rotate_by = Entry(axis_frame, textvariable=sRotateBy, width=3).pack(side=LEFT,padx=0, pady=0)
+        self.rotate_bz = Entry(axis_frame, textvariable=sRotateBz, width=3).pack(side=LEFT,padx=0, pady=0)
+
+        self.degree_label = Label(axis_frame, text="Degree:").pack(side=LEFT,padx=0,pady=0)
+        self.degree_spinbox = Spinbox(axis_frame, from_=0, to=360, width=3).pack(side=LEFT, padx=0, pady=0)
+
+        self.steps_rotation_label = Label(axis_frame, text="Steps:").pack(side=LEFT,padx=0,pady=0)
+        self.steps_rotation_spinbox = Spinbox(axis_frame, from_=0, to=10, width=3).pack(side=LEFT, padx=0, pady=0)
+
+        self.rotate_button = Button(axis_frame, text="Rotate", fg="blue", command=self.rotate)
+        self.rotate_button.pack(side=LEFT)                         
+
+        scale_frame = Frame(master.ob_root_window)
+        scale_frame.pack()
+
+        i_scale = IntVar(value=1)
+        self.scale_label = Label(scale_frame, text="Scale Ratio:").pack(side=LEFT,padx=0,pady=0)
+        self.scale_all = Radiobutton(scale_frame, text="All", variable=i_scale, value=1).pack(side=LEFT,anchor=W)
+
+        self.degree_spinbox = Spinbox(scale_frame, values=(0.25,0.50,0.75,1.00,1.25,1.50,1.75,2.00,2.25,2.50,2.75,3.00,3.25,3.50,3.75,4.00), width=3).pack(side=LEFT, padx=0, pady=0)
+        self.scale_sxsysz = Radiobutton(scale_frame, text="[Sx,Sy,Sz]", variable=i_scale, value=2).pack(side=LEFT,anchor=W)
+
+        fScaleSx = fDefaultOne
+        sScaleSx = StringVar(value=str(round(fScaleSx,2)))
+        fScaleSy = fDefaultOne
+        sScaleSy = StringVar(value=str(round(fScaleSy,2)))
+        fScaleSz = fDefaultOne
+        sScaleSz = StringVar(value=str(round(fScaleSz,2)))
+
+        fScaleAx = fDefaultZero
+        sScaleAx = StringVar(value=str(round(fScaleAx,2)))
+        fScaleAy = fDefaultZero
+        sScaleAy = StringVar(value=str(round(fScaleAy,2)))
+        fScaleAz = fDefaultZero
+        sScaleAz = StringVar(value=str(round(fScaleAz,2)))
+        
+        self.scale_sx = Entry(scale_frame, textvariable=sScaleSx, width=3).pack(side=LEFT,padx=0, pady=0)
+        self.scale_sy = Entry(scale_frame, textvariable=sScaleSy, width=3).pack(side=LEFT,padx=0, pady=0)
+        self.scale_sz = Entry(scale_frame, textvariable=sScaleSz, width=3).pack(side=LEFT,padx=0, pady=0)
+
+        self.scale_a_label = Label(scale_frame, text="Center of Scale:").pack(side=LEFT,padx=0,pady=0)
+
+        self.scale_ax = Entry(scale_frame, textvariable=sScaleAx, width=3).pack(side=LEFT,padx=0, pady=0)
+        self.scale_ay = Entry(scale_frame, textvariable=sScaleAy, width=3).pack(side=LEFT,padx=0, pady=0)
+        self.scale_az = Entry(scale_frame, textvariable=sScaleAz, width=3).pack(side=LEFT,padx=0, pady=0)
+
+        self.steps_rotation_label = Label(scale_frame, text="Steps:").pack(side=LEFT,padx=0,pady=0)
+        self.steps_rotation_spinbox = Spinbox(scale_frame, from_=0, to=10, width=3).pack(side=LEFT, padx=0, pady=0)
+
+        self.scale_button = Button(scale_frame, text="Scale", fg="blue", command=self.scale)
+        self.scale_button.pack(side=LEFT)                         
+
 
         
     def browse_file(self):
@@ -176,6 +258,12 @@ class cl_pannel_03:
             self.master.ob_world.create_graphic_objects(self.master.ob_canvas_frame.canvas,self.mesh)
 
             print ( "called the draw callback!")
+
+        def rotate(self):
+
+
+        def scale(self):
+            
 
 class MyDialog(simpledialog.Dialog):
     def body(self, master):
