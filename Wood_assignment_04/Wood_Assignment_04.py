0e880cecd83d736c113b1791ca2fbd2a72ba5c9e
WoodMath <jeffwood314@gmail.com>
2016-04-08 22:00:46 -0500

    Reset original 'assignment','graphics','widgets' files due to convoluted code from first 3 assignments

---
+++ Wood_assignment_04/Wood_Assignment_04.py
@@ -0,0 +1,25 @@
+# Wood, Jeff
+# 100-103-5461
+# 2016-04-19
+# Assignment_04
+
+from tkinter import *
+from tkinter import messagebox
+from tkinter import simpledialog
+from tkinter import filedialog
+
+import string
+from Wood_Widgets_04 import *
+from Wood_Graphics_04 import *
+
+def close_window_callback(root):
+    if messagebox.askokcancel("Quit", "Do you really wish to quit?"):
+        root.destroy()
+
+
+ob_root_window = Tk()
+ob_root_window.protocol("WM_DELETE_WINDOW", lambda root_window=ob_root_window: close_window_callback(root_window))
+ob_world=cl_world()
+cl_widgets(ob_root_window,ob_world)
+ob_root_window.mainloop()    
+    
---
+++ Wood_assignment_04/old_Wood_Graphics_04.py
@@ -0,0 +1,180 @@
+# Wood, Jeff
+# 100-103-5461
+# 2016-04-19
+# Assignment_04
+
+#   From:
+#       http://infohost.nmt.edu/tcc/help/pubs/tkinter/web/create_polygon.html
+#       http://infohost.nmt.edu/tcc/help/pubs/tkinter/web/create_line.html
+#       http://zetcode.com/gui/tkinter/drawing/
+#       http://effbot.org/tkinterbook/canvas.htm
+
+import numpy as np
+from math import *
+from tkinter import *
+from copy import *
+
+class cl_world:
+    def __init__(self, mesh, objects=[],canvases=[],lines=[],borders=[],edges=[]):
+        self.objects=objects
+        self.canvases=canvases
+        self.lines=lines
+        self.borders=borders
+        self.edges=edges
+        self.mesh=mesh
+        self.canvas=None
+        
+ 
+    def add_canvas(self,canvas):
+        self.canvases.append(canvas)
+        canvas.world=self
+    
+    def create_graphic_objects(self,canvas,mesh):
+        self.lines=[]                                # Array storing Triangles and Quads
+        self.borders=[]                                 # Array storing Viewport Box
+        self.edges=[]
+        
+        canvas.delete('all')                            # Clear screen of all objects
+        if(not len(mesh.vertices) and not (mesh.something2draw)):         # If no objects do not attempt to draw.
+            return
+
+        # Establish proper coordinates based on window size
+#        mesh.establish_screen_coordinates(canvas.cget("width"),canvas.cget("height"))
+
+
+        # DRAW viewport box
+        for i in range(0,len(mesh.box)-1):
+            self.borders.append(canvas.create_line(int(mesh.box[i,0]), int(mesh.box[i,1]), \
+                                                   int(mesh.box[i+1,0]), int(mesh.box[i+1,1]), \
+                                                   fill="black", width=1.0))
+        
+        ## DRAW all faces
+        for f in range(0,len(mesh.faces)):
+
+            v_line=[]
+            i_one = mesh.faces[f][0]
+            i_two = mesh.faces[f][1]
+            b_draw = mesh.faces[f][2]
+            v_to_add_one = mesh.screen_coordinates[i_one,:]
+            v_to_add_two = mesh.screen_coordinates[i_two,:]
+            
+#            v_line.append(int(v_to_add_one[0,0]))
+#            v_line.append(int(v_to_add_one[0,1]))
+#            v_line.append(int(v_to_add_two[0,0]))
+#            v_line.append(int(v_to_add_two[0,1]))
+            
+            v_line = [v_to_add_one[0,0],v_to_add_one[0,1],v_to_add_two[0,0],v_to_add_two[0,1]]
+
+            ## For each face loop over all indices to build
+            ## [[x0,y0],[x1,y1],[x2,y2],..[xn,yn]] array
+#            for i in range(0,len(mesh.faces[f])):
+#                i_index= mesh.faces[f][i]
+
+#                i_indices.append(i_index)
+
+#                v_to_add = mesh.screen_coordinates[i_index,:]
+                
+#                v_line.append(int(v_to_add[0,0]))
+#                v_line.append(int(v_to_add[0,1]))
+
+
+            ## DRAW actual faces
+#            self.lines.append(canvas.create_polygon(list(v_line), fill='red', width=1.0, outline='black'))
+
+            ## DRAW actual lines
+            if(b_draw):
+                self.lines.append(canvas.create_line(v_line, width=1.0, fill='black'))
+            else:
+                self.lines.append(None)
+
+
+    def redisplay_graphic_objects(self,canvas,event):
+        mesh=self.mesh
+        print(' Redisplay called ')
+
+        if(not len(mesh.vertices) and (not mesh.something2draw)):         # If no objects do not attempt to draw.
+            return
+        
+#        # If there are drawn objects
+#        if(self.borders or self.lines):
+#            # RE-Establish proper coordinates based on window size
+#            mesh.establish_screen_coordinates(canvas.cget("width"),canvas.cget("height"))
+            
+        # REPOSITION viewport box object
+        if self.borders:
+            for i in range(0,len(mesh.box)-1):
+                canvas.coords(\
+                    self.borders[i],\
+                    int(mesh.box[i,0]), int(mesh.box[i,1]),\
+                    int(mesh.box[i+1,0]), int(mesh.box[i+1,1])\
+                )
+
+        if self.lines:
+            ## REPOSITION all faces
+            for f in range(0,len(mesh.faces)):
+                v_line=[]
+                i_indices=[]
+
+                i_one = mesh.faces[f][0]
+                i_two = mesh.faces[f][1]
+                b_draw = mesh.faces[f][2]
+                v_to_add_one = mesh.screen_coordinates[i_one,:]
+                v_to_add_two = mesh.screen_coordinates[i_two,:]
+                v_line = [v_to_add_one[0,0],v_to_add_one[0,1],v_to_add_two[0,0],v_to_add_two[0,1]]
+
+                ## If line should be drawn
+                if(b_draw):
+                    ## If already drawn
+                    if(self.lines[f] != None):
+                        ## Simply change the coordinaes
+                        canvas.coords(self.lines[f], v_line)
+                    else:
+                        ## Otherwise store line at current buffer
+                        self.lines[f] = canvas.create_line(v_line, width=1.0, fill='black')
+                else: ## Otherwise
+                    ## If previously in buffer
+                    if(self.lines[f] != None):
+                        ## Delete object
+                        canvas.delete(self.lines[f])
+                        ## And its place in the buffer
+                        self.lines[f]=None
+                        
+#                ## For each face loop over all indices to build
+#                ## [[x0,y0],[x1,y1],[x2,y2],..[xn,yn]] array
+#                for i in range(0,len(mesh.faces[f])):
+#                    i_index= mesh.faces[f][i]
+#                    i_indices.append(i_index)
+#                    v_to_add = mesh.screen_coordinates[i_index,:]
+#
+#                    v_line.append(v_to_add[0,0])
+#                    v_line.append(v_to_add[0,1])
+#
+#                ## REPOSITION actual faces
+#                canvas.coords(self.lines[f], v_line)
+            canvas.update()
+
+    def clear(self,canvas,event=None):
+        mesh=self.mesh
+        print(' Clear called ')
+
+        canvas.delete('all')
+        canvas.delete(ALL)
+        
+        # DELETE viewport box object
+        if self.borders:
+            for i in range(0,len(self.borders)):
+                self.borders[i]
+                canvas.delete(self.borders[i])
+
+        if self.lines:
+
+            ## DELETE all lines
+            for i in range(0,len(self.lines)):
+                j=self.lines[i]
+                k=canvas.delete(self.lines[i])
+
+        self.borders=[]
+        self.lines=[]
+        #canvas.update()
+
+
