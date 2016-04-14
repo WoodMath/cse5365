3d00014ed9f23b26dbb3099b7e0eb1359b08f7fc
JeffWood <jeffwood314@gmail.com>
2016-04-14 16:59:49 -0500

    Additional Clipping changes

--- Wood_assignment_04/test.py
+++ Wood_assignment_04/test.py
@@ -1,12 +1,15 @@
 import numpy as np
 
-from Wood_Object_04 import *
-from Wood_Scene_04 import *
-from Wood_Camera_04 import *
-from Wood_Renderer_04 import *
+from Wood_Object_04_test import *
 
-o1 = Object()
+
+o = Object()
 #o1.loadFile('C:/Users/Jeff/Dropbox/cse5365/assignments/Wood_assignment_04/cube_04.txt')
-o1.loadFile('cube_04.txt')
+o.loadFile('teapot_04.txt')
 #print(' o.get() = ' + str(o.get()))
 
+#o.addIndice([1,9,10,2])
+#o.addIndice([2,10,11,3])
+#o.addIndice([3,11,12,4])
+
+print(' len(o.lineIndices) = ' + str(len(o.lineIndices)))
